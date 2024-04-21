1a9fb0340bcfa5c45bc2ba1c838b23bdb6bd7c77
bleedinglight <halpin2003@gmail.com>
2024-04-21 14:40:35 +0100

    balls

---
+++ Assets/UI/Scripts/DiscoveredAnimals.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DiscoveredAnimals", menuName = "ScriptableObjects/DiscoveredAnimals", order = 1)]
+
+public class DiscoveredAnimals : ScriptableObject
+{
+    
+    //some list of type animals
+    
+
+
+
+}
