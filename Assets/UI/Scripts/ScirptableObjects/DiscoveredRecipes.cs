e7f253a6717cf756e4b57a1ff2d5517b8194aacc
bleedinglight <halpin2003@gmail.com>
2024-04-22 16:02:00 +0100

    inv slide

--- Assets/UI/Scripts/ScirptableObjects/DiscoveredRecipes.cs
+++ Assets/UI/Scripts/ScirptableObjects/DiscoveredRecipes.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+
+[CreateAssetMenu(fileName = "DiscovereedRecipes", menuName = "ScriptableObjects/DiscoveredRecipes", order = 3)]
+public class DiscoveredRecipes : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
